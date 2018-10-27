425d8b9aa8c896c17f8b0fae72a6757ad76fc23c
fenrir7asteron <b.fedotov@innopolis.ru>
2018-10-28 01:46:12 +0300

    Added check boxes that toggle curves. Also added fields for changing initial values x0, y0, X and step. These fields do not affect the actual parameters yet.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,8 @@
 #include <qwt_plot_picker.h>
 #include <qwt_picker_machine.h>
 
-#include <utility>
+#include <qlineedit.h>
+#include <qtoolbutton.h>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -31,8 +32,13 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QwtPlot *d_plot = new QwtPlot( this );
+    setToolBar1();
+    setToolBar2();
+
+    d_plot = new QwtPlot( this );
     setCentralWidget(d_plot);
+
+    d_plot->setMinimumWidth(750);
 
     d_plot->setTitle("Approximate plot of DE"); // name of the plot
     d_plot->setCanvasBackground(Qt::white); // background color
@@ -43,7 +49,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
     d_plot->setAxisScale(QwtPlot::yLeft, 0, 40);
 
-    d_plot->insertLegend( new QwtLegend() );
+    QwtLegend *legend = new QwtLegend();
+    d_plot->insertLegend( legend, QwtPlot::BottomLegend );
 
     // turn on a grid
     QwtPlotGrid *grid = new QwtPlotGrid();
@@ -51,23 +58,23 @@ MainWindow::MainWindow(QWidget *parent) :
     grid->attach( d_plot ); // attach grid to a plot
 
     // create curves for different approximations and attach them to the plot
-    eulers_approximation_curve *euler_curve = new eulers_approximation_curve();
+    euler_curve = new eulers_approximation_curve();
     euler_curve->attach_to_plot(d_plot);
 
-    improved_eulers_approximation_curve *improved_euler_curve = new improved_eulers_approximation_curve();
+    improved_euler_curve = new improved_eulers_approximation_curve();
     improved_euler_curve->attach_to_plot(d_plot);
 
-    runge_kutta_approximation_curve *runge_kutta_curve = new runge_kutta_approximation_curve();
+    runge_kutta_curve = new runge_kutta_approximation_curve();
     runge_kutta_curve->attach_to_plot(d_plot);
 
     // create curves for different approximation errors and attach them to the plot
-    eulers_error_curve *euler_error = new eulers_error_curve();
+    euler_error = new eulers_error_curve();
     euler_error->attach_to_plot(d_plot);
 
-    improved_eulers_error_curve *improved_euler_error = new improved_eulers_error_curve();
+    improved_euler_error = new improved_eulers_error_curve();
     improved_euler_error->attach_to_plot(d_plot);
 
-    runge_kutta_error_curve *runge_kutta_error = new runge_kutta_error_curve();
+    runge_kutta_error = new runge_kutta_error_curve();
     runge_kutta_error->attach_to_plot(d_plot);
 
     //enable scaling with rotation of middle mouse button
@@ -94,6 +101,119 @@ MainWindow::MainWindow(QWidget *parent) :
 
     // attach new state machine that will check whether we drag plot or not
     d_picker->setStateMachine( new QwtPickerDragPointMachine() );
+
+    // create check boxes for toggling curves;
+    box1 = new QCheckBox( toolBar1 );
+    add_box(box1, "Euler approximation");
+    connect(box1, SIGNAL(toggled(bool)), SLOT(toggle_euler_approximation(bool)));
+
+    box2 = new QCheckBox( toolBar1 );
+    add_box(box2, "Improved Euler approximation");
+    connect(box2, SIGNAL(toggled(bool)), SLOT(toggle_improved_euler_approximation(bool)));
+
+    box3 = new QCheckBox( toolBar1 );
+    add_box(box3, "Runge-Kutta approximation");
+    connect(box3, SIGNAL(toggled(bool)), SLOT(toggle_runge_kutta_approximation(bool)));
+
+    box4 = new QCheckBox( toolBar1 );
+    add_box(box4, "Euler error");
+    connect(box4, SIGNAL(toggled(bool)), SLOT(toggle_euler_error(bool)));
+
+    box5 = new QCheckBox( toolBar1 );
+    add_box(box5, "Improved Euler error");
+    connect(box5, SIGNAL(toggled(bool)), SLOT(toggle_improved_euler_error(bool)));
+
+    box6 = new QCheckBox( toolBar1 );
+    add_box(box6, "Runge-Kutta error");
+    connect(box6, SIGNAL(toggled(bool)), SLOT(toggle_runge_kutta_error(bool)));
+
+    // create fields for entering initial values (x0, y0, X, step)
+    QLabel *x0_label = new QLabel(toolBar2);
+    add_label(x0_label, "x0");
+
+    QLineEdit *x0 = new QLineEdit();
+    add_line_edit(x0);
+
+    QLabel *y0_label = new QLabel(toolBar2);
+    add_label(y0_label, "y0");
+
+    QLineEdit *y0 = new QLineEdit();
+    add_line_edit(y0);
+
+    QLabel *X_label = new QLabel(toolBar2);
+    add_label(X_label, "X");
+
+    QLineEdit *X = new QLineEdit();
+    add_line_edit(X);
+
+    QLabel *step_label = new QLabel(toolBar2);
+    add_label(step_label, "step");
+
+    QLineEdit *step = new QLineEdit();
+    add_line_edit(step);
+}
+
+void MainWindow::setToolBar1()
+{
+    toolBar1 = new QToolBar( this );
+    toolBar1->setMovable(false);
+    addToolBar( toolBar1 );
+}
+
+void MainWindow::setToolBar2()
+{
+    toolBar2 = new QToolBar(this );
+    toolBar2->setMovable(false);
+    addToolBar(  Qt::RightToolBarArea, toolBar2 );
+}
+
+void MainWindow::add_box(QCheckBox *box, QString text)
+{
+    box->setText( text );
+    box->setCheckable( true );
+    box->setChecked(true);
+
+    toolBar1->addWidget( box ); // добавить кнопку на панель инструментов
+}
+
+void MainWindow::add_label(QLabel *label, QString text) {
+    label->setText(text);
+    toolBar2->addWidget(label);
+}
+
+void MainWindow::add_line_edit(QLineEdit *line) {
+    line->setMaximumWidth(80);
+    toolBar2->addWidget(line);
+}
+
+void MainWindow::toggle_euler_approximation(bool is_checked) {
+    is_checked ? euler_curve->show() : euler_curve->hide();
+    d_plot->replot();
+}
+
+void MainWindow::toggle_improved_euler_approximation(bool is_checked) {
+    is_checked ? improved_euler_curve->show() : improved_euler_curve->hide();
+    d_plot->replot();
+}
+
+void MainWindow::toggle_runge_kutta_approximation(bool is_checked) {
+    is_checked ? runge_kutta_curve->show() : runge_kutta_curve->hide();
+    d_plot->replot();
+}
+
+void MainWindow::toggle_euler_error(bool is_checked) {
+    is_checked ? euler_error->show() : euler_error->hide();
+    d_plot->replot();
+}
+
+void MainWindow::toggle_improved_euler_error(bool is_checked) {
+    is_checked ? improved_euler_error->show() : improved_euler_error->hide();
+    d_plot->replot();
+}
+
+void MainWindow::toggle_runge_kutta_error(bool is_checked) {
+    is_checked ? runge_kutta_error->show() : runge_kutta_error->hide();
+    d_plot->replot();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCheckBox>
+#include <QLabel>
+#include <QLineEdit>
+
+#include "approximation_curve.h"
+#include "eulers_approximation_curve.h"
+#include "improved_eulers_approximation_curve.h"
+#include "runge_kutta_approximation_curve.h"
+#include "eulers_error_curve.h"
+#include "improved_eulers_error_curve.h"
+#include "runge_kutta_error_curve.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +28,37 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QwtPlot *d_plot;
+    QToolBar *toolBar1;
+    QToolBar *toolBar2;
+
+    QCheckBox *box1;
+    QCheckBox *box2;
+    QCheckBox *box3;
+    QCheckBox *box4;
+    QCheckBox *box5;
+    QCheckBox *box6;
+
+    eulers_approximation_curve *euler_curve;
+    improved_eulers_approximation_curve *improved_euler_curve;
+    runge_kutta_approximation_curve *runge_kutta_curve;
+    eulers_error_curve *euler_error;
+    improved_eulers_error_curve *improved_euler_error;
+    runge_kutta_error_curve *runge_kutta_error;
+
+    void setToolBar1();
+    void setToolBar2();
+    void add_box(QCheckBox *box, QString text);
+    void add_label(QLabel *label, QString text);
+    void add_line_edit(QLineEdit *line);
+
+private Q_SLOTS:
+    void toggle_euler_approximation(bool is_checked);
+    void toggle_improved_euler_approximation(bool is_checked);
+    void toggle_runge_kutta_approximation(bool is_checked);
+    void toggle_euler_error(bool is_checked);
+    void toggle_improved_euler_error(bool is_checked);
+    void toggle_runge_kutta_error(bool is_checked);
 };
 
 #endif // MAINWINDOW_H
